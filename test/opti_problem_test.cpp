622c9fab9d63d7269f8b5569b9adb899ffe0a371
Ryan Day <rddays@gmail.com>
2020-09-24 19:52:27 -0500

    Change input to lvalue

--- test/opti_problem_test.cpp
+++ test/opti_problem_test.cpp
@@ -5,7 +5,8 @@ TEST(OptiProblem, VerifyRosenbrockObjectiveFunction) {
     int number_of_parameters = 2;
     std::vector<std::vector<double> > constraints{{-10, 10}, {-10, 10}};
     EvoAlgos::OptimizationProblem problem = EvoAlgos::OptimizationProblem(number_of_parameters, constraints);
-    double example_result = problem.objective_function(std::vector<double>{-1, 3});
+    std::vector<double> input{-1, 3};
+    double example_result = problem.objective_function(input);
     ASSERT_EQ(example_result, 404);
 }
 
