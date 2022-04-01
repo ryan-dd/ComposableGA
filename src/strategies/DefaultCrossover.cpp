fb3a44762322ea3f7a94dcf06f93b151d5af2a27
Ryan Day <rddays@gmail.com>
2022-03-31 23:23:50 -0500

    Fix bug in crossover

--- src/strategies/DefaultCrossover.cpp
+++ src/strategies/DefaultCrossover.cpp
@@ -1,8 +1,9 @@
 #include "DefaultCrossover.h"
+#include <iostream>
 
 DefaultCrossover::DefaultCrossover(DefaultCrossoverParams inputParamaters): 
         inputParams(inputParamaters),
-        intDistNumParams(0, inputParamaters.numParams),
+        intDistNumParams(0, inputParamaters.numParams - 1),
         realDistribution(0, 1),
         numberGenerator(std::random_device{}())
 {
@@ -19,12 +20,16 @@ void DefaultCrossover::crossover(std::vector<std::vector<entt::entity>>& input)
         {
             int crossover_index1 = intDistNumParams(numberGenerator);
             int crossover_index2 = intDistNumParams(numberGenerator);
+            auto beginIndex = std::min(crossover_index1, crossover_index2);
+            auto endIndex = std::max(crossover_index1, crossover_index2) + 1;
+            
             std::vector<entt::entity>& first = input[2*i];
             std::vector<entt::entity>& second = input[2*i+1];
+            
             std::swap_ranges(
-                first.begin() + std::min(crossover_index1, crossover_index2), 
-                first.begin() + std::max(crossover_index1, crossover_index2), 
-                second.begin());
+                first.begin() + beginIndex, 
+                first.begin() + endIndex, 
+                second.begin() + beginIndex);
         }
     }
 }
