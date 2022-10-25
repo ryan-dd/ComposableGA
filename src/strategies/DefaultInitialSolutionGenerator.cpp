e2df4e9ab006e4cfd453e6cf7e1c476097ea72a4
Ryan Day <rddays@gmail.com>
2022-10-24 22:49:25 -0500

    Update with better comments

--- src/strategies/DefaultInitialSolutionGenerator.cpp
+++ src/strategies/DefaultInitialSolutionGenerator.cpp
@@ -2,19 +2,24 @@
 
 std::vector<std::vector<entt::entity>> DefaultInitialSolutionGenerator::generate()
 {
-    std::vector<std::vector<entt::entity>> chromosomes;
+    std::vector<std::vector<entt::entity>> chromosomes{};
     chromosomes.resize(inputs.numChromosomes);
 
-    auto& registry = inputs.registry;
-    auto& parameters = inputs.parameters;
+    auto& registry{inputs.registry};
+    const auto& parameters{inputs.parameters};
     
-    for (auto i = 0; i < inputs.numChromosomes; ++i)
+    for (auto i{0}; i < inputs.numChromosomes; ++i)
     {
         for (auto j{0U}; j < parameters.size(); ++j)
         {
-            auto gene = registry.create();
-            registry.emplace<entt::entity>(gene, parameters[j]);
-            registry.get<ParameterFunctions>(parameters[j]).initializer(gene);
+            const auto gene{registry.create()};
+
+            // Associate the gene with its parameter type
+            // Parameter is also represented by an entt::entity
+            registry.emplace<entt::entity>(gene, parameters[j]); 
+
+            // Initialize the gene with the parameter type's initializer function
+            registry.get<ParameterFunctions>(parameters[j]).initializer(gene); 
             chromosomes.at(i).push_back(gene);
         }
     }
