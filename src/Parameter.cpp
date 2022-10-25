f0b7df1d48b0365b20921a776d0be55e4f828ae3
Ryan Day <rddays@gmail.com>
2022-10-24 23:09:09 -0500

    Continue const quest

--- src/Parameter.cpp
+++ src/Parameter.cpp
@@ -3,18 +3,18 @@
     
 entt::entity addParameter(ParameterFunctions paramFunctions, entt::registry& registry)
 {
-    auto parameter = registry.create();
+    const auto parameter{registry.create()};
     registry.emplace<ParameterFunctions>(parameter, paramFunctions);
     return parameter;
 }
 
 entt::entity addParameter(GeneratorFunction mutatorAndInitializerfunction, entt::registry& registry)
 {
-    auto parameter = registry.create();
-    auto functions = ParameterFunctions{
+    const auto parameter{registry.create()};
+    const auto functions{ParameterFunctions{
         .initializer = mutatorAndInitializerfunction, 
         .mutator = mutatorAndInitializerfunction
-    };
+    }};
     registry.emplace<ParameterFunctions>(parameter, functions);
     return parameter;
 }
