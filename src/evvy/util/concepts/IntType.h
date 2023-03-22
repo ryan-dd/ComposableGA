c8c61fd04bbfef27ad0d17331c70c1eef743cd70
Ryan Day <rddays@gmail.com>
2023-03-22 17:09:41 -0500

    Update to check type more accurately

--- src/evvy/util/concepts/IntType.h
+++ src/evvy/util/concepts/IntType.h
@@ -1,20 +1,21 @@
 #ifndef EVVY_INT_TYPE_H
 #define EVVY_INT_TYPE_H
 
+#include <type_traits>
 namespace evvy 
 {
 
 template<typename T>
 concept IntType = requires
 {
-  std::is_same_v<T, short> ||
-  std::is_same_v<T, int> ||
-  std::is_same_v<T, long> ||
-  std::is_same_v<T, long long> ||
-  std::is_same_v<T, unsigned short> ||
-  std::is_same_v<T, unsigned int> ||
-  std::is_same_v<T, unsigned long> ||
-  std::is_same_v<T, unsigned long long>;
+  std::is_same_v<std::remove_cvref_t<T>, short> ||
+  std::is_same_v<std::remove_cvref_t<T>, int> ||
+  std::is_same_v<std::remove_cvref_t<T>, long> ||
+  std::is_same_v<std::remove_cvref_t<T>, long long> ||
+  std::is_same_v<std::remove_cvref_t<T>, unsigned short> ||
+  std::is_same_v<std::remove_cvref_t<T>, unsigned int> ||
+  std::is_same_v<std::remove_cvref_t<T>, unsigned long> ||
+  std::is_same_v<std::remove_cvref_t<T>, unsigned long long>;
 };
 
 }
