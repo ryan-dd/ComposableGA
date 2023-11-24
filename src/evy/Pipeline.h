9842383d3a8106b5a0df3f4ab7204b5e4d7a7909
Ryan Day <rddays@gmail.com>
2023-11-24 13:12:19 -0600

    Updated StopCondition
    The StopCondition type in the template parameters wasn't actually
    needed, so I changed it to be an abbreviated function template.

--- src/evy/Pipeline.h
+++ src/evy/Pipeline.h
@@ -2,15 +2,12 @@
 #define EVY_PIPELINE_H
 
 #include <concepts>
-#include <algorithm>
-#include <functional>
-#include <initializer_list>
 
 namespace evy
 {
 
 template<typename T>
-concept stop_condition = requires(T func)
+concept StopCondition = requires(T func)
 {
   {func()} -> std::convertible_to<bool>;
 };
@@ -20,18 +17,17 @@ class Pipeline
 public:
   template<
     typename ChromosomeContainer,
-    stop_condition StopCondition,
     std::invocable<ChromosomeContainer&>... PipelineFunctions
   >
   static void run(
       ChromosomeContainer&& chromosomes, 
-      StopCondition&& shouldStop, 
+      StopCondition auto&& shouldStop, 
       PipelineFunctions&&... functions)
   {
     while(true)
     {
       // Invokes each function in left to right order
-      (std::forward<PipelineFunctions>(functions)(chromosomes), ...);
+      (std::forward<PipelineFunctions>(functions)(std::forward<ChromosomeContainer>(chromosomes)), ...);
 
       if(shouldStop())
       {
