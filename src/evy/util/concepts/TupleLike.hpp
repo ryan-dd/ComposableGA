b04c8bc69bc4d9af6b35b66965b4f1034ed42227
Ryan Day <rddays@gmail.com>
2023-11-24 21:00:46 -0600

    Update TupleLike concept
    Make the type be passed in with std::remove_cvref, also add some comments so
    that its a little easier to follow

--- src/evy/util/concepts/TupleLike.hpp
+++ src/evy/util/concepts/TupleLike.hpp
@@ -1,34 +1,53 @@
 #ifndef EVY_TUPLE_LIKE_H
 #define EVY_TUPLE_LIKE_H
 
+#include <type_traits>
+
 namespace evy
 {
 
 namespace detail
 {
+
 template<class T, std::size_t N>
-concept tuple_element_at_index =
+concept TupleElementAtIndex =
   requires(T t) 
   {
-    typename std::tuple_element_t<N, std::remove_const_t<T>>;
-    { get<N>(t) } -> std::convertible_to<const std::tuple_element_t<N, T>&>;
+    // tuple_element_t is defined for N
+    typename std::tuple_element_t<N, T>;
+
+    // get<N>(t) returns a reference that is the same type as tuple_element_t
+    // Allow ADL for get
+    { get<N>(t) } -> std::same_as<std::tuple_element_t<N, T>&>;
   };
-}
 
 template<class T>
-concept TupleLike = !std::is_reference_v<T> && 
+concept TupleLike =
   requires(T t) 
-  { 
+  {
+    // tuple_size::type is defined
     typename std::tuple_size<T>::type; 
+    // tuple_size_v returns std::size_t
+    { std::tuple_size_v<T> } -> std::same_as<std::size_t>;
+
+    // tuple_size inherits from integral_constant<std::size_t, N>
     requires std::derived_from<
       std::tuple_size<T>, 
       std::integral_constant<std::size_t, std::tuple_size_v<T>>
     >;
   } && 
+  // checks each tuple element has required functionality
   []<std::size_t... N>(std::index_sequence<N...>) 
   { 
-    return (detail::tuple_element_at_index<T, N> && ...); 
+    return (detail::TupleElementAtIndex<T, N> && ...); 
   }(std::make_index_sequence<std::tuple_size_v<T>>());
+
 }
+
+template<class T>
+concept TupleLike = detail::TupleLike<std::remove_cvref_t<T>>;
+
+}
+
 
 #endif
