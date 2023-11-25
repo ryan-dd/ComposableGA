44d13cecc9fcff80497f415365db2fe28a3d5c10
Ryan Day <rddays@gmail.com>
2023-11-24 21:07:23 -0600

    Update concepts formatting and fix bug
    Forgot a requires for std::is_aggregate_v in concept

--- src/evy/util/concepts/ValueMatching.hpp
+++ src/evy/util/concepts/ValueMatching.hpp
@@ -9,21 +9,21 @@ namespace evy
 template<typename T, typename ValueType>
 concept IndexableWithValue = requires(T container)
 {
-  {container[0]} -> std::convertible_to<ValueType>;
+  { container[0] } -> std::convertible_to<ValueType>;
 };
 
 template<typename Aggregate, typename ValueType, std::size_t Index>
 concept AggregateWithMemberTypeAtIndex = requires (Aggregate a)
 {
-  std::is_aggregate_v<Aggregate>;
-  {boost::pfr::get<Index>(a)} -> std::convertible_to<ValueType>;
+  requires std::is_aggregate_v<Aggregate>;
+  { boost::pfr::get<Index>(a) } -> std::convertible_to<ValueType>;
 };
 
 template<typename Tuple, typename ValueType, std::size_t Index>
-concept TupleWithMemberTypeAtIndex = requires (Tuple a)
+concept TupleWithMemberTypeAtIndex = requires (Tuple tuple)
 {
   requires evy::TupleLike<Tuple>;
-  {std::get<Index>(a)} -> std::convertible_to<ValueType>;
+  { get<Index>(tuple) } -> std::convertible_to<ValueType>;
 };
 
 }
