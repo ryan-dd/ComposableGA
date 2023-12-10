28fbfb6015c644926caaf91aebf10ef5298bd6eb
Ryan Day <rddays@gmail.com>
2023-12-09 18:57:44 -0600

    Format file and refactor
    For readability and line length

--- src/evy/util/ConditionallyApplyFunctions.hpp
+++ src/evy/util/ConditionallyApplyFunctions.hpp
@@ -12,28 +12,34 @@ namespace detail
 
 template<std::size_t index, typename InputType>
 requires TupleLike<InputType> || std::is_aggregate_v<InputType>
-constexpr void conditionallyApplyFunction(InputType& input, TupleLike auto& tupleOfFunctions, std::invocable auto& condition)
+constexpr void conditionallyApplyFunction(
+    InputType& input, 
+    TupleLike auto& tupleOfFunctions, 
+    std::invocable auto& condition)
 {
-    if(!condition())
-    {
-        return;
-    }
+  if(!condition())
+  {
+    return;
+  }
 
-    if constexpr (TupleLike<InputType>)
-    {
-        using std::get;
-        get<index>(tupleOfFunctions)(get<index>(input));
-    }
-    else if constexpr(std::is_aggregate_v<InputType>)
-    {
-        using std::get;
-        get<index>(tupleOfFunctions)(boost::pfr::get<index>(input));
-    }  
+  if constexpr (TupleLike<InputType>)
+  {
+    get<index>(tupleOfFunctions)(get<index>(input));
+  }
+  else if constexpr(std::is_aggregate_v<InputType>)
+  {
+    get<index>(tupleOfFunctions)(boost::pfr::get<index>(input));
+  }  
 }
 
 
 template <typename InputType, std::size_t... Idx>
-constexpr void conditionallyApplyFunctions(InputType& input, TupleLike auto& tupleOfFunctions, std::invocable auto& condition, std::index_sequence<Idx...>) {
+constexpr void conditionallyApplyFunctions(
+    InputType& input, 
+    TupleLike auto& tupleOfFunctions, 
+    std::invocable auto& condition, 
+    std::index_sequence<Idx...>) 
+{
     (conditionallyApplyFunction<Idx>(input, tupleOfFunctions, condition), ...);
 }
 
@@ -42,22 +48,35 @@ constexpr void conditionallyApplyFunctions(InputType& input, TupleLike auto& tup
 template <typename InputType, TupleLike TupleOfFunctions>
 requires (TupleLike<InputType> || std::is_aggregate_v<InputType>)
 // TODO validate TupleOfFunctions function type at each tuple index can operate on input type at same index
-constexpr void conditionallyApplyFunctions(InputType& input, TupleOfFunctions& tupleOfFunctions, std::invocable auto& condition) {
-    
-    if constexpr (TupleLike<InputType>)
-    {
-        static_assert(
-            std::tuple_size_v<InputType> == std::tuple_size_v<TupleOfFunctions>,
-            "There must be enough functions for each tuple arg of the input");
-        detail::conditionallyApplyFunctions(input, tupleOfFunctions, condition, std::make_index_sequence< std::tuple_size_v<InputType> >{});
-    }
-    else if constexpr (std::is_aggregate_v<InputType>)
-    {
-        static_assert(
-            boost::pfr::tuple_size_v<InputType> == std::tuple_size_v<TupleOfFunctions>,
-            "There must be enough functions for each member of the input");
-        detail::conditionallyApplyFunctions(input, tupleOfFunctions, condition, std::make_index_sequence< boost::pfr::tuple_size_v<InputType> >{});
-    }
+constexpr void conditionallyApplyFunctions(
+    InputType& input, 
+    TupleOfFunctions& tupleOfFunctions, 
+    std::invocable auto& condition) 
+{
+  if constexpr (TupleLike<InputType>)
+  {
+    static_assert(
+        std::tuple_size_v<InputType> == std::tuple_size_v<TupleOfFunctions>,
+        "There must be enough functions for each tuple arg of the input");
+    constexpr auto tupleSize{ std::tuple_size_v<InputType> };
+    detail::conditionallyApplyFunctions(
+        input, 
+        tupleOfFunctions, 
+        condition, 
+        std::make_index_sequence<tupleSize>{});
+  }
+  else if constexpr (std::is_aggregate_v<InputType>)
+  {
+    static_assert(
+        boost::pfr::tuple_size_v<InputType> == std::tuple_size_v<TupleOfFunctions>,
+        "There must be enough functions for each member of the input");
+    constexpr auto tupleSize{ boost::pfr::tuple_size_v<InputType> };
+    detail::conditionallyApplyFunctions(
+        input, 
+        tupleOfFunctions, 
+        condition, 
+        std::make_index_sequence<tupleSize>{});
+  }
 }
 
 }
